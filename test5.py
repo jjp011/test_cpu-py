13c9e4327fa417219ded7c7d4b841fb0869e83b5
Juan Jose Padilla <127537907+jjp011@users.noreply.github.com>
2024-01-24 20:08:42 +0100

    Create test5.py TIC and TAC

---
+++ test5.py
@@ -0,0 +1,39 @@
+import signal
+
+numTicks = 0
+
+def display_message(sig, frame):
+        #print(f"{sig=}")
+        #print(f"{frame=}")
+        global numTicks
+
+        numTicks += 1
+        print(f"TIC {numTicks=}")
+        signal.alarm(1)
+
+def tic(sig, frame):
+    print("TIC")
+    signal.signal(signal.SIGALRM, tac)
+    signal.alarm(1)
+
+
+def tac(sig, frame):
+    print("TAC")
+    signal.signal(signal.SIGALRM, tic)
+    signal.alarm(1)
+
+def main():
+    signal.signal(signal.SIGALRM, tic)
+    signal.alarm(1)
+
+    MAXCOUNT = 1000000000000
+    cont = 0
+    for i in range(MAXCOUNT):
+        cont += 1
+        if i % 1000000000 == 0:
+            print(f"{cont:10d}")
+    return 0
+
+if __name__ == "__main__":
+    main()
+  
